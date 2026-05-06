ffbae8a262ac917dc1842ce08ea22b7c9056a343
XVTOLIV1 <xvtoliv1@github.com>
2026-05-06 12:44:24 +0100

    build: configure barrel exports for all components, tokens, and utilities

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-export {};
+// Components
+export { Button } from "./components/Button";
+export type { ButtonProps, ButtonVariant, ButtonSize } from "./components/Button";
+
+export { Input } from "./components/Input";
+export type { InputProps, InputSize } from "./components/Input";
+
+export { Card, CardHeader, CardBody, CardFooter } from "./components/Card";
+export type { CardProps, CardHeaderProps, CardBodyProps, CardFooterProps, CardVariant } from "./components/Card";
+
+export { Badge } from "./components/Badge";
+export type { BadgeProps, BadgeVariant, BadgeSize } from "./components/Badge";
+
+export { Avatar, AvatarGroup } from "./components/Avatar";
+export type { AvatarProps, AvatarGroupProps, AvatarSize } from "./components/Avatar";
+
+export { Modal } from "./components/Modal";
+export type { ModalProps, ModalSize } from "./components/Modal";
+
+export { Tooltip } from "./components/Tooltip";
+export type { TooltipProps, TooltipPosition } from "./components/Tooltip";
+
+export { Toggle } from "./components/Toggle";
+export type { ToggleProps, ToggleSize } from "./components/Toggle";
+
+export { Select } from "./components/Select";
+export type { SelectProps, SelectOption } from "./components/Select";
+
+export { Skeleton, SkeletonCard } from "./components/Skeleton";
+export type { SkeletonProps, SkeletonVariant } from "./components/Skeleton";
+
+export { Alert } from "./components/Alert";
+export type { AlertProps, AlertVariant } from "./components/Alert";
+
+export { Divider } from "./components/Divider";
+export type { DividerProps, DividerOrientation } from "./components/Divider";
+
+// Utilities
+export { cn } from "./utils/cn";
